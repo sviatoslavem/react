e29cc73e69fc7d83b7b2f2ea5ad04f4d4d2d3da8
Sviatoslav <sviatoslavem@gmail.com>
2026-01-09 18:57:33 +0200

    H.W. 2

--- lesson_4/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Posts from "./components/Posts/Posts";
-const App = () => {
-  return <Posts />;
-};
-
-export default App;
--- lesson_4/src/components/Button/Button.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Button = ({ title, handleClick }) => {
-  return <button onClick={handleClick}>{title}</button>;
-};
-
-export default Button;
